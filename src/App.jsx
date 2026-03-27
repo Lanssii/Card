2e91dee0af89c642a8c18bb6f3e32de5fe6eff97
lana shotashvili <shotashvililana@gmail.com>
2026-03-27 18:18:17 +0400

    Fetch and display motivational facts from JSON data in Card components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "./components/Card";
 import "./App.css";
 
 function App() {
+  const [facts, setFacts] = useState([]);
+
+  useEffect(() => {
+    fetch("/data.json")
+      .then((res) => res.json())
+      .then((data) => setFacts(data.facts))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="w-full h-screen flex items-center justify-center bg-[#6D94C5]">
-      <Card number="1" />
+      <div className="w-full max-w-5xl flex flex-wrap justify-center items-stretch gap-6 p-4">
+        {facts.map((fact, index) => (
+          <Card
+            key={fact.id}
+            number={index + 1}
+            title={fact.title}
+            text={fact.text}
+          />
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,16 @@
 export default function card(props) {
   return (
-    <div className="bg-[#CBDCEB] relative rounded-3xl rounded-br-3xl pt-6 pr-10 pb-12 pl-6 ">
-      <h1 className="text-black-900 font-bold text-xl">Subscribe</h1>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt velit
-        qui molestiae doloribus tempora, officiis obcaecati exercitationem nisi
-        libero placeat, ad ut, quo omnis quaerat dignissimos non inventore ea
-        distinctio!
-      </p>
-      <div className="bg-transparent z-1  w-4 h-4 rounded-br-lg absolute right-0 bottom-8 shadow-[4px_4px_0_rgba(109,148,197,1)]"></div>
-      <div className="bg-transparent absolute bottom-0 right-8 rounded-br-lg w-4 h-4 shadow-[4px_4px_0_rgba(109,148,197,1)]"></div>
-      <div className="absolute bottom-0 right-0 pt-1 pl-1  flex justify-end items-end bg-[#6D94C5] rounded-tl-2xl">
-        <div className="bg-[#CBDCEB] rounded-full w-7 h-7 flex justify-center items-center text-black-900 font-semibold text-xl">
+    <div
+      className={`bg-[#CBDCEB] relative rounded-3xl rounded-br-3xl pt-6 pr-10 pb-12 pl-6 h-[250px] ${
+        props.number % 2 === 0 ? "w-[500px]" : "w-[350px]"
+      }`}
+    >
+      <h1 className="text-black-900 font-bold text-xl mb-4">{props.title}</h1>
+      <p>{props.text}</p>
+      <div className="bg-transparent z-1 w-8 h-8 rounded-br-2xl absolute right-0 bottom-11 shadow-[10px_2px_0_rgba(109,148,197,1)]"></div>
+      <div className="bg-transparent absolute bottom-0 right-11 z-2 rounded-br-2xl w-6 h-6 shadow-[2px_10px_0_rgba(109,148,197,1)]"></div>
+      <div className="absolute bottom-0 right-0 pt-1 pl-1  flex justify-end items-end bg-[#6D94C5] rounded-tl-3xl">
+        <div className="bg-[#CBDCEB] rounded-full w-10 h-10 flex justify-center items-center text-black-900 font-semibold text-xl">
           {props.number}
         </div>
       </div>
